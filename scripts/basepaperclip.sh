0673cce2a0ddc92e6a1cd48c8b735afd1fb542c5
Peridot <peridot491@protonmail.com>
2022-06-06 19:36:26 +0200

    Fix paperclip

--- scripts/basepaperclip.sh
+++ scripts/basepaperclip.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
-cd ./Paperclip
-git fetch origin master
-git reset --hard 2d4c7b3
-cd ..
-cp ../../Titanium-Server/target/titanium*-SNAPSHOT.jar ./Paperclip/titanium-1.8.8.jar
+cp ../../Titanium-Server/build/libs/titanium-1.8.8-R0.1-SNAPSHOT.jar ./Paperclip/titanium-1.8.8.jar
 cp ./work/1.8.8/1.8.8.jar ./Paperclip/minecraft_server.1.8.8.jar
 cd ./Paperclip
-mvn -Dmcver=1.8.8 -Dvanillajar=../minecraft_server.1.8.8.jar -Dpaperjar=../titanium-1.8.8.jar clean package
+sed -i -e 's/http:\/\/clojars/https:\/\/clojars/g' ./java8/pom.xml # Replace http repository with https
+mvn clean package --batch-mode -Dmcver=1.8.8 "-Dpaperjar=../titanium-1.8.8.jar" "-Dvanillajar=../minecraft_server.1.8.8.jar"
 cd ..
-cp ./Paperclip/assembly/target/paperclip-1.8.8.jar ./titanium-paperclip.jar
+cp ./Paperclip/assembly/target/paperclip*.jar ../../Titanium-Server/build/libs/titanium.jar
+
+cd ../../Titanium-Server/build/libs
 
 echo ""
 echo ""
 echo ""
 echo "Build success!"
-echo "Copied final jar to $(pwd)/titanium-paperclip.jar"
+echo "Copied final jar to $(pwd)/titanium.jar"
